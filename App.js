04d4f31c7f21c20090fd08c25ceb6478527071fa
Awadhesh786 <awadheshchaurasia786@gmail.com>
2019-07-17 01:23:26 +0530

    logout done

--- App.js
+++ App.js
@@ -1,55 +1,12 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import RNAccountKit from 'react-native-facebook-account-kit';
+import React, { Component } from 'react';
+import Home from './src/screen/Home';
+import Log from './src/screen/Log';
 
-class App extends React.Component {
+class App extends Component{
   render(){
-
-    RNAccountKit.configure({
-      responseType: 'code', // 'token' by default,
-      // titleType: 'login',
-      // initialAuthState: '',
-      initialEmail: 'some.initial@email.com',
-      initialPhoneCountryPrefix: '+91', // autodetected if none is provided
-      // initialPhoneNumber: '123-456-7890',
-      // facebookNotificationsEnabled: true|false, // true by default
-       readPhoneStateEnabled: true, // true by default,
-      // countryWhitelist: ['AR'], // [] by default
-      // countryBlacklist: ['US'], // [] by default
-      defaultCountry: 'IN',
-      // theme: {
-      //   // for iOS only, see the Theme section
-      // },
-      // viewControllerMode: 'show'|'present', // for iOS only, 'present' by default
-       getACallEnabled: true,
-       setEnableInitialSmsButton: true, // true by default
-    })
-    
-    // Shows the Facebook Account Kit view for login via SMS
-    RNAccountKit.loginWithPhone()
-      .then((token) => {
-        if (!token) {
-          console.log('Login cancelled')
-        } else {
-          console.log(`Logged with phone. Token: ${token}`)
-        }
-      })
-    
-
     return(
-      <View style={{justifyContent:"center", alignSelf:"center", flex:1}}>
-        <Text style={{color:"red"}}>Hello ji </Text>
-      </View>
+      <Home />
     );
   }
-};
-
-
+}
 export default App;
---
+++ src/screen/Log.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import RNAccountKit from 'react-native-facebook-account-kit';
+
+class App extends React.Component {
+  render(){
+
+    RNAccountKit.configure({
+      responseType: 'token', // 'token' by default,
+      // titleType: 'login',
+      // initialAuthState: '',
+      initialEmail: 'some.initial@email.com',
+      initialPhoneCountryPrefix: '+91', // autodetected if none is provided
+      // initialPhoneNumber: '123-456-7890',
+      // facebookNotificationsEnabled: true|false, // true by default
+       readPhoneStateEnabled: true, // true by default,
+      // countryWhitelist: ['AR'], // [] by default
+      // countryBlacklist: ['US'], // [] by default
+      defaultCountry: 'IN',
+      // theme: {
+      //   // for iOS only, see the Theme section
+      // },
+      // viewControllerMode: 'show'|'present', // for iOS only, 'present' by default
+       getACallEnabled: true,
+       setEnableInitialSmsButton: true, // true by default
+    })
+    
+    // Shows the Facebook Account Kit view for login via SMS
+    RNAccountKit.loginWithPhone()
+      .then((token) => {
+        if (!token) {
+          console.log('Login cancelled')
+        } else {
+          console.log(`Logged with phone. Token: ${token}`)
+        }
+      })
+    
+
+    return(
+      <View style={{justifyContent:"center", alignSelf:"center", flex:1}}>
+        <Text style={{color:"red"}}>Hello ji </Text>
+      </View>
+    );
+  }
+};
+
+
+export default App;
